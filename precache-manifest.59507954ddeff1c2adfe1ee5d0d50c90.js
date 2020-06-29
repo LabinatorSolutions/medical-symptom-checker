8bb01af4f21ac48ca8a0b9c70dcda78a5bb30d9d
Labinator Team <info@labinator.com>
2020-06-29 14:52:20 +0800

    Updates

---
+++ precache-manifest.59507954ddeff1c2adfe1ee5d0d50c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd676d90121bf51a05e11639fc0bcfa1",
+    "url": "/medical-symptom-checker/index.html"
+  },
+  {
+    "revision": "0d12b7f012d97107b37a",
+    "url": "/medical-symptom-checker/static/css/main.69ccaf17.chunk.css"
+  },
+  {
+    "revision": "93c5ac818eaa303b8687",
+    "url": "/medical-symptom-checker/static/js/2.d6faa787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/medical-symptom-checker/static/js/2.d6faa787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d12b7f012d97107b37a",
+    "url": "/medical-symptom-checker/static/js/main.31ba06f0.chunk.js"
+  },
+  {
+    "revision": "125fc0659366ac43ea55",
+    "url": "/medical-symptom-checker/static/js/runtime-main.215e28c6.js"
+  },
+  {
+    "revision": "7c583c79e665fe726210608b348137e1",
+    "url": "/medical-symptom-checker/static/media/logo.7c583c79.svg"
+  }
+]);
